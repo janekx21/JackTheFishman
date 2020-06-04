e042a98f9da7ec826f9a821edb352a44e264a39b
Hannes Winkler <hanneswinkler2000@web.de>
2020-06-04 15:31:00 +0200

    Vector2f implementation - add `Vector2fConst` - add `Vector2fCopy` - add `clamp` and `times` for `Vector2f` inside `Extensions.kt` - resolves #6

---
+++ src/engine/math/Vector2fCopy.kt
@@ -0,0 +1,13 @@
+import org.joml.Vector2f
+
+object Vector2fCopy {
+    val zero: Vector2f get() = Vector2f(0f, 0f)
+    val one: Vector2f get() = Vector2f(1f, 1f)
+
+    // val forward: Vector3f get() = Vector3f(0f, 0f, -1f)
+    // val backwards: Vector3f get() = Vector3f(0f, 0f, 1f)
+    val right: Vector2f get() = Vector2f(1f, 0f)
+    val left: Vector2f get() = Vector2f(-1f, 0f)
+    val up: Vector2f get() = Vector2f(0f, 1f)
+    val down: Vector2f get() = Vector2f(0f, -1f)
+}
