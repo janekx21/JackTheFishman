784b4c01450f340edc5495e6504d9b6b26206699
Janek Winkler <janekx21@gmail.com>
2020-07-03 11:00:22 +0200

    gravity getter & setter

--- src/engine/Physics.kt
+++ src/engine/Physics.kt
@@ -1,15 +1,24 @@
 package engine
 
+import engine.math.toVec2
+import engine.math.toVector2fc
 import org.jbox2d.common.Vec2
 import org.jbox2d.dynamics.World
+import org.joml.Vector2fc
 
 object Physics {
     var velocityIterations = 6
     var positionIterations = 2
 
-    val world = World(Vec2(0f, 1f))
+    val world = World(Vec2(0f, 0f))
 
     fun update() {
         world.step(Time.deltaTime, velocityIterations, positionIterations)
     }
+
+    var gravity: Vector2fc
+        get() = world.gravity.toVector2fc()
+        set(value) {
+            world.gravity = value.toVec2()
+        }
 }
