11bb995e82dba5b8565e2e67d96651984e4d871b
schaumbu <49246642+schaumbu@users.noreply.github.com>
2020-07-06 15:35:34 +0200

    Update HealthComponent.kt
    code fix

--- DodgyDeliveries3/src/HealthComponent.kt
+++ DodgyDeliveries3/src/HealthComponent.kt
@@ -4,19 +4,19 @@ class HealthComponent(gameObject: GameObject) : Component(gameObject) {
 
     var maxHp: Float = 0F
 
-    public fun takeDamage(dmg: Float){
-        if (hp > 0){
+    public fun takeDamage(dmg: Float) {
+        if (hp > 0) {
             hp -= dmg
-            if(hp < 0){
+            if (hp < 0) {
                 hp = 0F
             }
         }
     }
 
     public fun heal(heal: Float) {
-        if(hp < maxHp) {
+        if (hp < maxHp) {
             hp += heal
-            if(hp > maxHp) {
+            if (hp > maxHp) {
                 hp = maxHp
             }
         }
