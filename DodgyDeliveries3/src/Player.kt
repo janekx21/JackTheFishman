5ed0b7c8a325fade938b054ebb27c65f9904cae4
ArneS2k20 <arne.schaumburg@st.ovgu.de>
2020-07-06 15:12:22 +0200

    fixed player controller

--- DodgyDeliveries3/src/Player.kt
+++ DodgyDeliveries3/src/Player.kt
@@ -1,17 +1,18 @@
+import components.BoxCollider
+import components.ModelRenderer
+import components.Transform
 import engine.Loader
 import engine.graphics.Mesh
-import engine.graphics.Shader
 
 object Player : GameObject("player") {
-    private val diffuseshader = Loader.createViaPath(Shader, "shaders/diffuse.shader")
 
     init {
         addComponent<Transform>()
         addComponent<ModelRenderer>().also {
             it.mesh = Loader.createViaPath(Mesh, "models/monkey.fbx") // TODO: add player mesh
-            it.shader = diffuseshader
         }
-        addComponent<ColliderComponent>()
+        addComponent<BoxCollider>()
         addComponent<PlayerController>()
+        Scene.active.spawn(Player)
     }
 }
--- DodgyDeliveries3/src/PlayerController.kt
+++ DodgyDeliveries3/src/PlayerController.kt
@@ -1,7 +1,9 @@
+import components.Collider
 import engine.Input
 import org.joml.Vector2f
 
 class PlayerController(gameObject: GameObject) : Component(gameObject) {
+
     override fun update() {
         controller()
         friction()
@@ -10,17 +12,17 @@ class PlayerController(gameObject: GameObject) : Component(gameObject) {
     override fun draw() {
     }
 
-    private val speed = 1f
+    private val speed = 0.001f
 
     // provisionally only supports mouse -- TODO: add keyboard and controller
     fun controller() {
-        val col = gameObject.getComponent<ColliderComponent>()
+        val col = gameObject.getComponent<Collider>()
         col.velocity = Vector2f(col.velocity.x() + Input.Mouse.deltaPosition.x * speed, col.velocity.y())
     }
 
     // provisionally friction -- very bad code
     fun friction() {
-        val col = gameObject.getComponent<ColliderComponent>()
+        val col = gameObject.getComponent<Collider>()
         col.velocity = Vector2f(col.velocity.x() * 0.9f, col.velocity.y() * 0.9f)
         if (col.velocity.x() < 0.01f && col.velocity.x() > -0.01f) {
             col.velocity = Vector2f(0f, col.velocity.y())
