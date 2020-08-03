4e2c09212537cdb54ebee6e1c27a2ec97591832e
Bennet Meier <bennet.meier@gmx.net>
2020-08-03 15:37:06 +0200

    set hp on Start and reload when dead

--- DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/HealthComponent.kt
+++ DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/HealthComponent.kt
@@ -1,10 +1,16 @@
 package dodgyDeliveries3.components
 
 import dodgyDeliveries3.Component
+import dodgyDeliveries3.Scene
+import dodgyDeliveries3.loadDefaultScene
 
 class HealthComponent : Component() {
     var hp: Float = 0F
     var maxHp: Float = 0F
+    
+    fun Start(){
+        hp = 10F
+    }
 
     fun takeDamage(dmg: Float) {
         if (hp > 0) {
@@ -22,6 +28,11 @@ class HealthComponent : Component() {
                 hp = maxHp
             }
         }
+    }
+
+    fun die(){
+        //ToDo: Find better solution
+        loadDefaultScene()
     }
 
     override fun update() {}
