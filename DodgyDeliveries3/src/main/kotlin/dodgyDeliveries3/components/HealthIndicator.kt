6196595161a406f07bc63f1a5df4eee67a44379b
Bennet Meier <bennet.meier@gmx.net>
2020-08-29 15:55:59 +0200

    display pizza

--- DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/HealthIndicator.kt
+++ DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/HealthIndicator.kt
@@ -3,6 +3,7 @@ package dodgyDeliveries3.components
 import dodgyDeliveries3.Component
 import jackTheFishman.engine.Loader
 import jackTheFishman.engine.graphics.Texture2D
+import org.joml.Vector2f
 import kotlin.math.floor
 
 class HealthIndicator(var images: Array<Texture2D> = emptyArray(), var healthComponent: Health? = null) : Component() {
@@ -12,15 +13,27 @@ class HealthIndicator(var images: Array<Texture2D> = emptyArray(), var healthCom
         return images[floor((images.size - 1) * healthComponent!!.percentage).toInt()]
     }
 
+
     override fun start() {
-        //TODO: fill array with images
         for(i in 0..images.size){
             images[i] = Loader.createViaPath<Texture2D>("textures/healthIndication/pizza" + i.toString() + ".png")
+        }
+        displayImage()
+    }
+
+    override fun update() {
+        //changeImage()
+    }
+
+    fun displayImage(){
+        gameObject.addComponent<ImageComponent>().also {
+            it.texture = image
+            it.scaledPosition = Vector2f(100f, 100f)
+            it.logicalSize = Vector2f(100f, 100f)
         }
     }
 
     fun changeImage(){
-        //TODO: iterate through image array
+        gameObject.getComponent<ImageComponent>().texture = image;
     }
-        
 }
