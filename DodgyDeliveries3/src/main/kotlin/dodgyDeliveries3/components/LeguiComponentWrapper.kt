beb97454b35546eb4ff0671d30417239acafefa3
Hannes Winkler <hanneswinkler2000@web.de>
2020-08-29 15:49:09 +0200

    some updates - add `onLayout` callback in LeguiComponentWrapper.kt that will be called:   - before the legui component is added to the GUI   - each time the window is resized - rename `position` & `scaledPosition` to `physicalPosition` and `logicalPosition`

--- DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/LeguiComponentWrapper.kt
+++ DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/LeguiComponentWrapper.kt
@@ -5,18 +5,37 @@ import dodgyDeliveries3.Scene
 import jackTheFishman.engine.Window
 import org.joml.Vector2f
 import org.joml.Vector2fc
+import org.joml.Vector2ic
 
-open class LeguiComponentWrapper<out T>(val leguiComponent: T, var onPressed: () -> Unit = {}) : Component() where T : org.liquidengine.legui.component.Component {
-    var position: Vector2fc
+open class LeguiComponentWrapper<T>(leguiComponent: T, var onPressed: () -> Unit = {}, val onLayout: ((LeguiComponentWrapper<T>) -> Unit)? = null) : Component() where T : org.liquidengine.legui.component.Component {
+    private var isEnabled: Boolean = false
+    private var isAddedToScene: Boolean = false
+    private var physicalSizeLastFrame: Vector2ic = Window.physicalSize
+
+    var leguiComponent: T = leguiComponent
+        set(value) {
+            if (isAddedToScene) {
+                removeFromScene()
+            }
+
+            wasPressedLastUpdate = false
+            field = value
+
+            if (isEnabled) {
+                addToScene()
+            }
+        }
+
+    var physicalPosition: Vector2fc
         get() = Vector2f(leguiComponent.position)
         set(value) {
             leguiComponent.position = Vector2f(value)
         }
 
-    var scaledPosition: Vector2fc
-        get() = Vector2f(position).div(Window.contentScale)
+    var logicalPosition: Vector2fc
+        get() = Vector2f(physicalPosition).div(Window.contentScale)
         set(value) {
-            position = Vector2f(value).mul(Window.contentScale)
+            physicalPosition = Vector2f(value).mul(Window.contentScale)
         }
 
     /**
@@ -40,17 +59,44 @@ open class LeguiComponentWrapper<out T>(val leguiComponent: T, var onPressed: ()
 
     private var wasPressedLastUpdate = false
 
+    private fun layout() {
+        if (onLayout != null) {
+            onLayout!!(this)
+        }
+    }
+
+    private fun addToScene() {
+        check(isEnabled)
+
+        layout()
+        Scene.active.rootPanel.add(leguiComponent)
+        isAddedToScene = true
+    }
+
+    private fun removeFromScene() {
+        check(isAddedToScene)
+
+        Scene.active.rootPanel.remove(leguiComponent)
+    }
+
     override fun start() {
         super.start()
-        Scene.active.rootPanel.add(leguiComponent)
+        isEnabled = true
+        addToScene()
     }
 
     override fun stop() {
-        Scene.active.rootPanel.remove(leguiComponent)
+        removeFromScene()
+        isEnabled = false
         super.stop()
     }
 
     override fun update() {
+        if (Window.physicalSize != physicalSizeLastFrame) {
+            layout()
+            physicalSizeLastFrame = Window.physicalSize
+        }
+
         if (leguiComponent.isPressed && !wasPressedLastUpdate) {
             onPressed()
             wasPressedLastUpdate = true
