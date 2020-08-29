21c3143fc562d4dea510c0028a802a0915e03953
Hannes Winkler <hanneswinkler2000@web.de>
2020-08-29 17:23:35 +0200

    HealthIndicator updates Component now uses `setOrigin` to set internalGameObject

--- DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/HealthIndicator.kt
+++ DodgyDeliveries3/src/main/kotlin/dodgyDeliveries3/components/HealthIndicator.kt
@@ -1,41 +1,57 @@
 package dodgyDeliveries3.components
 
 import dodgyDeliveries3.Component
+import dodgyDeliveries3.GameObject
 import dodgyDeliveries3.Scene
 import jackTheFishman.engine.Loader
 import jackTheFishman.engine.graphics.Texture2D
 import org.joml.Vector2f
-import kotlin.math.floor
+import org.joml.Vector4f
+import org.liquidengine.legui.style.Background
+import org.liquidengine.legui.style.Border
+import kotlin.math.roundToInt
 
-class HealthIndicator(var images: Array<Texture2D> = emptyArray(), var healthComponent: Health? = null) : Component() {
-
-    val image : Texture2D
-    get() {
-        return images[floor((images.size - 1) * healthComponent!!.percentage).toInt()]
+class HealthIndicator : Component() {
+    private val images: List<Texture2D> = (0 .. 5).map {
+        Loader.createViaPath<Texture2D>("textures/healthIndication/pizza$it.png")
     }
 
-
-    override fun start() {
-        for(i in 0..images.size){
-            images[i] = Loader.createViaPath<Texture2D>("textures/healthIndication/pizza" + i.toString() + ".png")
+    private val image : Texture2D?
+        get() {
+            if (healthComponent == null) {
+                return null
+            } else {
+                val imageIndex = (images.size - 1) - ((images.size - 1) * healthComponent!!.percentage).roundToInt()
+                return images[imageIndex]
+            }
         }
-        displayImage()
-        healthComponent = Scene.active.findViaName("Player")?.getComponent<Health>()
+
+    private val healthComponent: Health?
+        get() = Scene.active.findViaName("Player")?.getComponent()
+
+    private val imageComponent: ImageComponent = ImageComponent().also {
+        if (image != null) {
+            it.texture = image
+        }
+        it.leguiComponent.style.also { style ->
+            style.background.color = Vector4f(0f, 0f, 0f, 0f)
+            style.border.isEnabled = false
+            style.shadow.color = Vector4f(0f, 0f, 0f, 0f)
+        }
+        it.scaledPosition = Vector2f(8f, 8f)
+        it.logicalSize = Vector2f(100f, 100f)
+    }
+
+    override fun setOrigin(origin: GameObject) {
+        super.setOrigin(origin)
+        origin.addComponent(imageComponent)
     }
 
     override fun update() {
-        changeImage()
+        updateImage()
     }
 
-    fun displayImage(){
-        gameObject.addComponent<ImageComponent>().also {
-            it.texture = image
-            it.scaledPosition = Vector2f(100f, 100f)
-            it.logicalSize = Vector2f(100f, 100f)
-        }
-    }
-
-    fun changeImage(){
-        gameObject.getComponent<ImageComponent>().texture = image;
+    private fun updateImage() {
+        imageComponent.texture = image
     }
 }
